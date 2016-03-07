03706dc500fea2470347ce352920afb833a088cf
emcleod17 <emcleod@wisc.edu>
2016-03-07 15:36:12 -0600

    Initial commit

---
+++ app/src/androidTest/java/com/example/emcleod/cs407_android_hw2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.emcleod.cs407_android_hw2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
